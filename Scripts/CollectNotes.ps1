25f0e6596b3585f749e38208587589878a155458
wharvex <t.gudlewski@gmail.com>
2024-06-16 12:23:52 -0400

    add notes scripts from powershell 5 scripts folder

---
+++ Scripts/CollectNotes.ps1
@@ -0,0 +1,56 @@
+﻿<#PSScriptInfo
+
+.VERSION 0.1
+
+.AUTHOR Tim Gudlewski
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS Concatenate Notes Text Files
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+First version
+
+#> 
+
+<# 
+
+.DESCRIPTION 
+Take the contents of multiple *.txt files in the current directory and 
+concatenate them all into one file, and open that file in vim. Pass in how many
+days ago you want to start collecting file contents from.
+
+#> 
+
+Function collect
+{
+    Param ([Parameter(Position=0, Mandatory=$false)] [int]$days_ago)
+
+    $x_days_ago = (Get-Date) - (New-TimeSpan -Days $days_ago)
+
+    $notes_dir_contents_since_x = Get-ChildItem -Filter "*.txt" | `
+    sort LastWriteTime | `
+    Where-Object { $_.CreationTime -ge $x_days_ago }
+
+    $out_path = "all_from_$($x_days_ago.ToString("yyyy-MM-dd(hh-mm-ss)"))_to_$(Get-Date -Format "yyyy-MM-dd(hh-mm-ss)")"
+
+    $notes_dir_contents_since_x | ForEach-Object `
+    { "`n******************* $($_.CreationTime) *******************`n`n$($(Get-Content $_) -Join "`n" )" } `
+    | Out-File -FilePath $out_path
+
+    vim $out_path
+}
